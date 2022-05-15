d55141c4ad5dff4abaefe1728e92fb8e75690ebe
Burak GÜL <burakkggul@gmail.com>
2022-05-15 14:17:09 +0300

    basics

---
+++ repeat-lessons/hello-java/variable-memory/src/com/bilgeadam/Manager.java
@@ -0,0 +1,14 @@
+package com.bilgeadam;
+
+public class Manager {
+    private static String name = "Burak";
+    private String surname = "GÜL";
+
+    public void printManagerSurname(){
+        System.out.println(name);
+    }
+
+    public static void printManagerNameWithStatic(){
+        System.out.println(name);
+    }
+}
