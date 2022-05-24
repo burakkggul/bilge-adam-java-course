41d6920a9fb8c0d307c7be5b39d62b8207cccc71
Burak GÜL <burak.gul@orionbt.com.tr>
2022-05-24 20:28:01 +0300

    repeat

--- repeat-lessons/hello-java/variable-memory/src/com/bilgeadam/Manager.java
+++ repeat-lessons/hello-java/variable-memory/src/com/bilgeadam/Manager.java
@@ -5,10 +5,22 @@ public class Manager {
     private String surname = "GÜL";
 
     public void printManagerSurname(){
-        System.out.println(name);
+        String a = "A";
+        System.out.println(surname);
+    }
+
+    public void printManagerNameAndSurname(){
+        String hello = "Hello Java";
+        for(int i = 0 ; i < 10 ; i++){
+            String notAccesable = "Not accesable";
+            System.out.println(hello);
+        }
+        // System.out.println(notAccesable);
     }
 
     public static void printManagerNameWithStatic(){
+        String a = "A";
         System.out.println(name);
+        a = "B";
     }
 }
