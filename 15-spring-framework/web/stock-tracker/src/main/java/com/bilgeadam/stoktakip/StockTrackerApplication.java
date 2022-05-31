3825f83055c6a4ec985efa70a816f97651f93382
Burak GÜL <burak.gul@orionbt.com.tr>
2022-05-31 19:11:02 +0300

    app name changed

--- 15-spring-framework/web/stock-tracker/src/main/java/com/bilgeadam/stoktakip/StockTrackerApplication.java
+++ 15-spring-framework/web/stock-tracker/src/main/java/com/bilgeadam/stoktakip/StockTrackerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StokTakipApplication {
+public class StockTrackerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StokTakipApplication.class, args);
+        SpringApplication.run(StockTrackerApplication.class, args);
     }
 
 }
