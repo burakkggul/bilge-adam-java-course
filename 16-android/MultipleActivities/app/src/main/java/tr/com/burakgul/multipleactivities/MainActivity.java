407eadbd1ae8cd743832cb1cd8227f474deab2fd
Burak GÜL <burak.gul@orionbt.com.tr>
2022-08-21 11:31:58 +0300

    multiple activity

--- 16-android/MultipleActivities/app/src/main/java/tr/com/burakgul/multipleactivities/MainActivity.java
+++ 16-android/MultipleActivities/app/src/main/java/tr/com/burakgul/multipleactivities/MainActivity.java
@@ -2,7 +2,9 @@ package tr.com.burakgul.multipleactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.setContentView(R.layout.activity_main);
+
+        Intent intent = super.getIntent();
+        System.out.println(intent.getCategories());
+    }
+
+    public void changeActivity(View view){
+        Intent intent = new Intent(super.getApplicationContext(),SecondActivity.class);
+        super.startActivity(intent);
     }
 }
