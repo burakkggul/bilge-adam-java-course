b98973fda027cbed9a16c19b222e95e7fe1dc490
Burak GÜL <burak.gul@orionbt.com.tr>
2022-08-21 10:25:11 +0300

    context

--- 16-android/alertmessage/app/src/main/java/tr/com/burakgul/alert_message/MainActivity.java
+++ 16-android/alertmessage/app/src/main/java/tr/com/burakgul/alert_message/MainActivity.java
@@ -3,6 +3,8 @@ package tr.com.burakgul.alert_message;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -15,18 +17,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this,"Aktivite başlatıldı.",Toast.LENGTH_LONG).show();
+        Context applicationContext = super.getApplicationContext();
+        Context mainActivityContext = this;
+        Toast.makeText(mainActivityContext,"Aktivite başlatıldı.",Toast.LENGTH_LONG).show();
+        Toast.makeText(applicationContext,"Uygulama başlatıldı.",Toast.LENGTH_LONG).show();
     }
 
     public void alert(View view){
         this.alert = new AlertDialog.Builder(this);
         alert.setTitle("Uyarı!");
         alert.setMessage("Emin misiniz?");
-        alert.setPositiveButton("Evet", (dialog, which) -> {
+        /*alert.setPositiveButton("Evet", (dialog, which) -> {
             Toast.makeText(this,"Evet dediniz.",Toast.LENGTH_LONG).show();
+        });*/
+        alert.setPositiveButton("Evet", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this,"Evet dediniz.",Toast.LENGTH_LONG).show();
+            }
         });
-        alert.setNegativeButton("Hayır.",(dialog,which) -> {
+        /*alert.setNegativeButton("Hayır.",(dialog,which) -> {
             Toast.makeText(this,"Hayır dediniz.",Toast.LENGTH_LONG).show();
+        });*/
+        alert.setNegativeButton("Hayır", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //Toast.makeText(MainActivity.super.getApplicationContext(),"Evet dediniz.", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(),"Evet dediniz.", Toast.LENGTH_LONG).show();
+            }
         });
     }
 
