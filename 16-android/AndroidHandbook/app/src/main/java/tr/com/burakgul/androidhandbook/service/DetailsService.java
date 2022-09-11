e3f493e36b787abf964109ddaa8871736799ba02
Burak GÜL <burak.gul@orionbt.com.tr>
2022-09-11 12:55:03 +0300

    android handbook listview

--- 16-android/AndroidHandbook/app/src/main/java/tr/com/burakgul/androidhandbook/service/DetailsService.java
+++ 16-android/AndroidHandbook/app/src/main/java/tr/com/burakgul/androidhandbook/service/DetailsService.java
@@ -2,9 +2,18 @@ package tr.com.burakgul.androidhandbook.service;
 
 import lombok.RequiredArgsConstructor;
 import tr.com.burakgul.androidhandbook.databinding.ActivityDetailsBinding;
+import tr.com.burakgul.androidhandbook.model.Concept;
 
 @RequiredArgsConstructor
 public class DetailsService {
 
     private final ActivityDetailsBinding activityDetailsBinding;
+
+    private final Concept concept;
+
+    public void initializeContent() {
+        this.activityDetailsBinding.titleTextView.setText(this.concept.getTitle());
+        this.activityDetailsBinding.conceptImageView.setImageResource(this.concept.getImage());
+        this.activityDetailsBinding.descriptionTextView.setText(this.concept.getDescription());
+    }
 }
