5dbc9058d0a0babaaba0e6cbc42a894a9b8ebea7
Burak GÜL <burak.gul@orionbt.com.tr>
2022-09-11 11:43:51 +0300

    android handbook listview

---
+++ 16-android/AndroidHandbook/app/src/main/java/tr/com/burakgul/androidhandbook/service/TableOfContentsService.java
@@ -0,0 +1,57 @@
+package tr.com.burakgul.androidhandbook.service;
+
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import tr.com.burakgul.androidhandbook.R;
+import tr.com.burakgul.androidhandbook.databinding.ActivityTableOfContentsBinding;
+import tr.com.burakgul.androidhandbook.model.Concept;
+
+@Getter
+@RequiredArgsConstructor
+public class TableOfContentsService {
+
+    private final ActivityTableOfContentsBinding activityTableOfContentsBinding;
+
+    private final List<Concept> concepts = new ArrayList<>();
+
+    public void prepareConcepts() {
+        Concept countDownTimer = new Concept("Count Down Timer",
+                R.drawable.countdowntimer,
+                "Bir countdown timer bu şekilde yazılabilir.");
+
+        Concept mockConcept2 = new Concept("Count Down Timer2",
+                R.drawable.countdowntimer,
+                "Bir countdown timer bu şekilde yazılabilir. 2");
+        Concept mockConcept3 = new Concept("Count Down Timer3",
+                R.drawable.countdowntimer,
+                "Bir countdown timer bu şekilde yazılabilir. 3");
+
+        Concept mockConcept4 = new Concept("Count Down Timer4",
+                R.drawable.countdowntimer,
+                "Bir countdown timer bu şekilde yazılabilir. 4");
+
+        Concept mockConcept5 = new Concept("Count Down Timer5",
+                R.drawable.countdowntimer,
+                "Bir countdown timer bu şekilde yazılabilir. 5");
+
+        this.concepts.add(countDownTimer);
+        this.concepts.add(mockConcept2);
+        this.concepts.add(mockConcept3);
+        this.concepts.add(mockConcept4);
+        this.concepts.add(mockConcept5);
+    }
+
+    public void prepareListViewAdapter(){
+        ArrayAdapter<Concept> arrayAdapter =
+                new ArrayAdapter<>(activityTableOfContentsBinding.getRoot().getContext(),
+                        R.layout.simple_list_item,
+                        this.concepts);
+        System.out.println(this.concepts.get(0).toString());
+        activityTableOfContentsBinding.androidHandbookList.setAdapter(arrayAdapter);
+    }
+}
